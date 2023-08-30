12486cfc617ddb0684bf17774bf9af04948086e6
nelsonkimaiga <nelsonkimaiga@aol.com>
2023-08-30 12:59:14 +0300

    First commit and code structure set-up

---
+++ src/main/java/com/intellisoft/findhai/FindHaiApplication.java
@@ -0,0 +1,13 @@
+package com.intellisoft.findhai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindHaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindHaiApplication.class, args);
+	}
+
+}
